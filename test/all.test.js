78d6c50d04ce9ae95db558bf46c3830c3888a513
redouanDZ <redhkobz@gmail.com>
2026-08-28 16:54:35 +0200

    chore(ci): drop node 18 matrix to fix cheerio undici crash, remove global dispatcher

--- test/all.test.js
+++ test/all.test.js
@@ -1,8 +1,5 @@
 /**
  * Master Test Suite - Runs all integration & remediation test suites
  */
-const { setGlobalDispatcher, Agent } = require('undici');
-setGlobalDispatcher(new Agent({ keepAliveTimeout: 10, keepAliveMaxTimeout: 10 }));
-
 require('./api.test.js');
 require('./remediation.test.js');
